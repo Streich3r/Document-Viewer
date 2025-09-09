6191a7c3f98b359c2287e5a6f25d24806c98b4b2
Streich3r <138706902+Streich3r@users.noreply.github.com>
2025-09-09 18:30:35 +0200

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "matrix-docs-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "pdf.js",
+  "mammoth.browser.min.js",
+  "xlsx.full.min.js",
+  "jszip.min.js",
+  "pptxjs.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
